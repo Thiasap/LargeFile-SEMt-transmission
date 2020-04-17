f3b126b22777b8ae767f4b2c913817e5d15a2bbd
Thiasap <799721744@qq.com>
2020-04-17 17:31:55 +0800

    init

---
+++ LargeFile-SEMt-transmission/LargeFile-SEMt-transmission.cpp
@@ -0,0 +1,12 @@
+﻿// LargeFile-SEMt-transmission.cpp: 定义应用程序的入口点。
+//
+
+#include "LargeFile-SEMt-transmission.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake。" << endl;
+	return 0;
+}
